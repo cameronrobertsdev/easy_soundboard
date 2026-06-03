7324ae5a020f611fb63745eee85e235adba242e8
Cameron Roberts <cameronroberts1998@gmail.com>
2026-06-03 14:33:24 -0700

    made yes button work

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const audioContext = new AudioContext();
 
-const yesAudioElement = document.querySelector("yesAudio");
-const noAudioElement = document.querySelector("noAudio");
+const yesAudioElement = document.querySelector("#yesAudio");
+const noAudioElement = document.querySelector("#noAudio");
 
-const yesTrack = audioContext.createMediaElementSource(yesAudioElement)
+const yesTrack = audioContext.createMediaElementSource(yesAudioElement);
+
+yesTrack.connect(audioContext.destination);
+
+const yesButton = document.querySelector("#yesButton");
+
+yesButton.addEventListener("pointerdown", () =>{
+
+    if(audioContext.state === "suspended"){
+        audioContext.resume();
+    }
+    if(yesButton.dataset.playing === "false"){
+        yesAudioElement.play();
+        yesButton.dataset.playing  = "true";
+    }
+    else if (yesButton.dataset.playing === "true"){
+        yesAudioElement.pause();
+        yesButton.dataset.playing = "false";
+    }
+
+});
+
+yesAudioElement.addEventListener("ended", () => {
+    yesButton.dataset.playing = "false";
+});
