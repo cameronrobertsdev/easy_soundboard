de427fdb5eb53ab94390f0bc4942b2816775e9e2
Cameron Roberts <cameronroberts1998@gmail.com>
2026-06-03 14:03:56 -0700

    added yes/no sounds

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const yesSound = Audio()
+const audioContext = new AudioContext();
+
+const yesAudioElement = document.querySelector("yesAudio");
+const noAudioElement = document.querySelector("noAudio");
+
+const yesTrack = audioContext.createMediaElementSource(yesAudioElement)
